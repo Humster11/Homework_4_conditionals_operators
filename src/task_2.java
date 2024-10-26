be2338f172fa7d0b0159f46034577d4b548dd937
daria.lapina <ms.humster11@gmail.com>
2024-10-26 10:23:43 +0300

    Initial commit

---
+++ src/task_2.java
@@ -0,0 +1,11 @@
+public class task_2 {
+    public static void main(String[] args) {
+        int temperature = 5;
+        if (temperature <= 5) {
+            System.out.println("На улице " + temperature + " градусов, нужно надеть шапку");
+        }
+        if (temperature > 5) {
+            System.out.println("На улице " + temperature + " можно идти без шапки");
+        }
+    }
+}
